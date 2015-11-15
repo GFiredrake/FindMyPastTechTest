f37977e0be56f2fd55ac3013234905c8dac73a4b
Robin Windon <rwindon@hotmail.co.uk>
2015-11-15 14:25:09 +0000

    Commit #2 User controls added

---
+++ FindMyPastTechTest.Tests/Tests/UserControlls.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using NUnit.Framework;
+
+namespace FindMyPastTechTest.Tests.Tests
+{
+    [TestFixture]
+    class UserControlls
+    {
+        FirefoxDriver firefox;
+        [SetUp]
+        public void Init()
+        { 
+            firefox = new FirefoxDriver(); 
+        }
+
+        [TearDown]
+        public void Dispose()
+        { 
+            firefox.Quit(); 
+        }
+
+        [Test]
+        public void Are_The_Number_Box_And_Button_Present()
+        {
+            firefox.Navigate().GoToUrl("http://localhost:5050/");
+            Assert.AreEqual(true, firefox.FindElement(By.Id("NumberBox")).Displayed);
+            Assert.AreEqual(true, firefox.FindElement(By.Id("GenerateButton")).Displayed); 
+        }
+    }
+}
